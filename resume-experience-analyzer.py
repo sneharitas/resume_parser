6a34ea36f8800ec72215d6887a53bc6b755ecb29
Sneha <020bscit042@sxc.edu.np>
2026-03-11 14:25:26 +0545

    Resume parser project

---
+++ resume-experience-analyzer.py
@@ -0,0 +1,9 @@
+experience = int(input("Enter years of AI experience: "))
+if experience < 1:
+    print("AI Beginner")
+elif experience <=3:
+    print(" Junior AI Engineer")
+elif experience <=7:
+    print("Mid Level AI Engineer")
+else:
+    print("Senior AI Engineer")
---
+++ resume-score-calculator.py
@@ -0,0 +1,14 @@
+score =0
+python = input(" Do you know Python? (yes/no): ")
+ml= input("Do you know Machine Learning? (yes/no): ")
+tensorflow = input("Do you know TensorFlow or PyTorch? (yes/no): ")
+
+if python == "yes":
+    score = score + 10
+
+if ml == "yes":
+    score = score + 10 
+
+if tensorflow == "yes":
+    score=score + 10
+print ("AI Resume Score: ", score)
