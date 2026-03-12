706228d79d85138362ff0ff1661fcc3d660a006e
sneharitas <103917898+sneharitas@users.noreply.github.com>
2026-03-12 07:48:52 +0545

    Update resume-project-counter.py

--- resume-project-counter.py
+++ resume-project-counter.py
@@ -1,12 +1,15 @@
-projects = int(input("How many AI projects are in your portfolio? "))
+def resume_project_counter()
+    projects = int(input("How many AI projects are in your portfolio? "))
 
-Total = []
+    Total = []
 
-for i in range(projects):
-    project = input("Enter AI project name: ")
-    Total.append(project)
+    for i in range(projects):
+        project = input("Enter AI project name: ")
+        Total.append(project)
 
-print("\nYour AI Projects:")
+    print("\nYour AI Projects:")
 
-for i in range(len(Total)):
-    print(i+1, ".", Total[i])
+    for i in range(len(Total)):
+        print(i+1, ".", Total[i])
+    if __name__ = "main":
+    main()
