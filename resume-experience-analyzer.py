1bfa6e605f255deaeb616ee29fe5eecb6893d102
sneharitas <103917898+sneharitas@users.noreply.github.com>
2026-03-12 07:47:59 +0545

    Update resume-experience-analyzer.py

--- resume-experience-analyzer.py
+++ resume-experience-analyzer.py
@@ -1,9 +1,14 @@
-experience = int(input("Enter years of AI experience: "))
-if experience < 1:
-    print("AI Beginner")
-elif experience <=3:
-    print(" Junior AI Engineer")
-elif experience <=7:
-    print("Mid Level AI Engineer")
-else:
-    print("Senior AI Engineer")
+def resume_experience_analyzer()
+
+    experience = int(input("Enter years of AI experience: "))
+    if experience < 1:
+        print("AI Beginner")
+    elif experience <=3:
+        print(" Junior AI Engineer")
+    elif experience <=7:
+        print("Mid Level AI Engineer")
+    else:
+        print("Senior AI Engineer")
+
+if __name__ = "main":
+main()
