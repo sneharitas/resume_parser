1d4a31f2099f46d80bdeb1f18fc15f69a1c25d13
sneharitas <103917898+sneharitas@users.noreply.github.com>
2026-03-12 07:46:45 +0545

    Update resume-eligibility-checker.py

--- resume-eligibility-checker.py
+++ resume-eligibility-checker.py
@@ -1,7 +1,13 @@
-name = input("Enter your name:")
-experience = int(input("Enter years of AI/ML experience:"))
-if experience>=3:
-    print(name, "is eligible for AI Software Engineer role")
-else:
-    print(name, "is eligible for AI intern or Junior ML Engineer role")
-    
+
+def resume_eligibility_checker()
+
+    name = input("Enter your name:")
+    experience = int(input("Enter years of AI/ML experience:"))
+    if experience>=3:
+        print(name, "is eligible for AI Software Engineer role")
+    else:
+        print(name, "is eligible for AI intern or Junior ML Engineer role")
+
+
+if __name__ = "main":
+main()
