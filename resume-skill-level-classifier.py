0ce0f55053999e4d1bf1d7131244b1070bf5c483
sneharitas <103917898+sneharitas@users.noreply.github.com>
2026-03-12 07:50:37 +0545

    Update resume-skill-level-classifier.py

--- resume-skill-level-classifier.py
+++ resume-skill-level-classifier.py
@@ -1,7 +1,10 @@
-skills = int(input("How many AI/ML skills do you have?"))
-if skills>=5:
-    print("Strong AI Engineer Profile")
-elif skills>=3:
-    print("Average AI Engineer Profile")
-else:
-    print("Begginer AI Learner")
+def resume_skill_level_classifier()
+    skills = int(input("How many AI/ML skills do you have?"))
+    if skills>=5:
+        print("Strong AI Engineer Profile")
+    elif skills>=3:
+        print("Average AI Engineer Profile")
+    else:
+        print("Begginer AI Learner")
+      if __name__ = "main":
+        main()
